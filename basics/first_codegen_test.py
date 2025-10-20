bb1bfaa60b35d114ac8b3b499a8b6e75eee735b9
Filip Gonda <filip.gonda1@gmail.com>
2025-10-20 20:15:08 +0200

    Initial commit of playwright pytest automation framework

---
+++ basics/first_codegen_test.py
@@ -0,0 +1,25 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://automationexercise.com/")
+    page.get_by_role("heading", name="AutomationExercise").click()
+    page.get_by_role("heading", name="Full-Fledged practice website").click()
+    page.get_by_role("link", name=" Signup / Login").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").fill("filip.gonda1@gmail.com")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("Test123")
+    page.get_by_role("button", name="Login").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
---
+++ pytest_fw/test_login_logout.py
@@ -0,0 +1,13 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://automationexercise.com/")
+    page.get_by_role("link", name=" Signup / Login").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").fill("filip.gonda1@gmail.com")
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("Test123")
+    page.get_by_role("button", name="Login").click()
+    page.get_by_role("link", name=" Logout").click()
