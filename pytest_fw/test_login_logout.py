db3c91d5cb32389434515cf4d30d77e1e0ed8de3
Filip Gonda <filip.gonda1@gmail.com>
2025-10-21 21:08:27 +0200

    Updated test with logout scenario, added gitignore file

--- pytest_fw/test_login_logout.py
+++ pytest_fw/test_login_logout.py
@@ -2,7 +2,17 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_example(page: Page) -> None:
+def test_login_positive(page: Page) -> None:
+    page.goto("https://automationexercise.com/")
+    page.get_by_role("link", name=" Signup / Login").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").click()
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").fill("filip.gonda1@gmail.com")
+    page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("Test123")
+    page.get_by_role("button", name="Login").click()
+    page.get_by_role("link", name=" Logout").click()
+
+def test_logout_positive(page: Page) -> None:
     page.goto("https://automationexercise.com/")
     page.get_by_role("link", name=" Signup / Login").click()
     page.locator("form").filter(has_text="Login").get_by_placeholder("Email Address").click()
